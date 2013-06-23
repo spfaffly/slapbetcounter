abda26561469291b408be7993d7a3e2969ed44ca
Shane Pfaffly <spfaffly@gmail.com>
2013-06-22 21:42:05 -0500

    Change countdown options

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -10,12 +10,10 @@ $(document).ready(function(){
 								color 	: "#FFFFFF",
 								weight	: "normal"	 // < - no comma on last item!
 							},
-							year  	: 2013,  // The target date's year (optional)
-							month 	: 7,     // The target date's month (optional)
-							day   	: 27,     // The target date's day (optional)
-							hour  	: 8,     // The target date's hour (optional)
-							ampm  	: "pm",	  // Is the hour refering to AM (early/day) or PM (late/night)?
-							minute	: 0,          // The target date's minutes (optional)
-							second	: 0,          // The target date's seconds (optional)
+							year  	: 2013,		// The target date's year (optional)
+							month 	: 7,		// The target date's month (optional)
+							day   	: 27,		// The target date's day (optional)
+							hour  	: 8,		// The target date's hour (optional)
+							ampm  	: "pm"		// Is the hour refering to AM (early/day) or PM (late/night)?
 						});
 });
